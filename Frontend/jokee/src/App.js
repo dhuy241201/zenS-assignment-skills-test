dc7ceb271abf56401e006198fb27a49ac8b618f1
dhuy241201 <d.huy241201@gmail.com>
2024-04-02 14:00:19 +0700

    update: jokee UI - responsive for mobile

--- Frontend/jokee/src/App.js
+++ Frontend/jokee/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './css/grid.css'
-import './css/responsive.css'
 import './css/main.css'
+import './css/responsive.css'
 import JokePage from './pages/JokePage';
 function App() {
   return (
