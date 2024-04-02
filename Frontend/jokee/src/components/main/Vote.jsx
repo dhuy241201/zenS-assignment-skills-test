8fe9cdbe379663fd6297e8a294d648bdd9a23b32
dhuy241201 <d.huy241201@gmail.com>
2024-04-02 11:14:24 +0700

    add: vote joke feature

--- Frontend/jokee/src/components/main/Vote.jsx
+++ Frontend/jokee/src/components/main/Vote.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 
-function Vote() {
+function Vote({jokeIndex,changeJokeIndex,voteManagement}) {
+
+  const handleVoteFunny = () => {
+    changeJokeIndex();
+    let listVote = voteManagement.votes
+    listVote.push(1);
+    voteManagement.setVote(listVote)
+  }
+
+  const handleVoteNotFunny = () => {
+    changeJokeIndex();
+    let listVote = voteManagement.votes
+    listVote.push(0);
+    voteManagement.setVote(listVote)
+  }
+
+
   return (
     <div id='vote'>
       <div className='vote__top-line'>
         <hr className='vote__top-line-content'/>
         </div>
       <div className="vote__container">
-        <div className="vote__button-group">
-          <button className="vote__button btn-funny">This is Funny!</button>
-          <button className="vote__button btn-not-funny">This is not funny.</button>
+        <div className="vote__button-group" disabled={jokeIndex>=4 ? 'true': 'false' }>
+          <button className="vote__button btn-funny"
+          onClick={handleVoteFunny}
+          disabled={jokeIndex >= 4}
+          style={{ backgroundColor:jokeIndex >= 4 ? "grey" : {}}}
+          >This is Funny!</button>
+          <button className="vote__button btn-not-funny"
+          onClick={handleVoteNotFunny}
+          disabled={jokeIndex >= 4}
+          style={{ backgroundColor:jokeIndex >= 4 ? "grey" : {}}}
+          >This is not funny.</button>
         </div>
       </div>
     </div>
--- Frontend/jokee/src/pages/JokePage.jsx
+++ Frontend/jokee/src/pages/JokePage.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie';
 import Header from '../components/common/Header'
 import Banner from '../components/main/Banner'
 import Jokes from '../components/main/Jokes'
 import Vote from '../components/main/Vote'
 import Footer from '../components/common/Footer'
-import '../css/main.css'
 
 function JokePage() {
+  const [jokeIndex, setJokeIndex] = useState(0);
+  const [voteList, setVoteList] = useState([]);
+  const [cookie, setCookie, removeCookie] = useCookies(['vote-list'])
+
+  useEffect(() => {
+    if (cookie['vote-list']) {
+      setVoteList(cookie['vote-list']);
+    }
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      const json_vote = JSON.stringify(voteList);
+      setCookie('vote-list', json_vote, { path: '/' })
+    }
+  }, [])
+
+  const handleChangeJokeIndex = () => {
+    let index = jokeIndex + 1;
+    setJokeIndex(index);
+  }
+
+  const voteManagement = {
+    votes: voteList,
+    setVote: setVoteList
+  }
+
   return (
     <div>
-        <Header></Header>
-        <Banner></Banner>
-        <Jokes></Jokes>
-        <Vote></Vote>
-        <Footer></Footer>
+      <Header></Header>
+      <Banner></Banner>
+      <Jokes index={jokeIndex}></Jokes>
+      <Vote jokeIndex={jokeIndex} changeJokeIndex={handleChangeJokeIndex} voteManagement={voteManagement}></Vote>
+      <Footer></Footer>
     </div>
   )
 }
